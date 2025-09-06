7be9bd38cd6ad3b9148c9e0ef46a488dfcac1331
Nasccped <pedro.main.tec@gmail.com>
2025-09-06 09:59:28 -0300

    feat(fix): add strategy interface

---
+++ src/strategy/Strategy.java
@@ -0,0 +1,9 @@
+package strategy;
+
+/**
+ * Strategy to be ran with specific types. (I've tried to use static,
+ * but I'd fail :^(     )
+ */
+public interface Strategy<Input, Output> {
+    public Output strategy(Input input);
+}
